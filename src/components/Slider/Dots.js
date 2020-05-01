c2ea933bb56876389c5ab87099de0a01c56c747b
Arleen Santana <santana.arleen@gmail.com>
2020-05-01 03:54:49 -0400

    Slider with autoplay

---
+++ src/components/Slider/Dots.js
@@ -0,0 +1,35 @@
+/** @jsx jsx */
+import React from 'react'
+import { css, jsx } from '@emotion/core'
+
+const Dot = ({ active }) => (
+  <span
+    css={css`
+      padding: 8px;
+      margin-right: 5px;
+      cursor: pointer;
+      border-radius: 50%;
+      opacity: .5;
+      background: ${active ? 'black' : 'white'};
+    `}
+  />
+)
+
+const Dots = ({ slides, activeIndex }) => (
+  <div
+    css={css`
+      position: absolute;
+      bottom: 25px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+  >
+    {slides.map((slide, i) => (
+      <Dot key={slide} active={activeIndex === i} />
+    ))}
+  </div>
+)
+
+export default Dots
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import Slider from '../components/Slider/Slider';
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+
+const images = [
+  'https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80',
+  'https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80',
+  'https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80',
+  'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
+]
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Slider slides={images} autoPlay={3} />
   </Layout>
 )
 
