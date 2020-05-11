8b50ee403eb6fdd4bd6619ecf6a379244e7749a0
Arleen Santana <santana.arleen@gmail.com>
2020-05-11 01:13:50 -0400

    updates to get involved section

--- src/components/Cards/FlipCard.js
+++ src/components/Cards/FlipCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BsPersonSquare} from "react-icons/bs";
 
 const FlipCard = (props) => {
-  console.log(props);
+  
   return(
     <div className="flipcard">
       <div className="flipcard__side flipcard__side--front">
